5a15a171f34f86f9bf0f737f089ba78266fb1856
ozkostay <ozkostay@mail.ru>
2022-10-02 21:15:49 +0500

    start 021022 2

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,15 +1,15 @@
 import calkhealth from '../calkhealth';
 
 const heroes = [
-  {name: 'мечник', health: 10},
-  {name: 'маг', health: 100},
-  {name: 'лучник', health: 80},
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
 ];
 
 const heroesSort = [
-  {name: 'маг', health: 100},
-  {name: 'лучник', health: 80},
-  {name: 'мечник', health: 10},
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
 ];
 
 test('testhomework', () => {
